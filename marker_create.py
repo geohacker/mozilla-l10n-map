de80dcdb38baa18916501acd6a6ccae6a409044e
Sajjad Anwar <me@sajjad.in>
2011-12-05 13:56:30 +0530

    Markers now contain neat links

--- marker_create.py
+++ marker_create.py
@@ -12,12 +12,20 @@ for row in data:
         lon = row[3]
         name = row[0]
         pop = row[1]
+        pop_list = pop.split("<br>")
+        print "processing "+name
+        print pop_list
+        team_page = pop_list[1]
+        try:
+            dashboard = pop_list[4]
+        except:
+            pass
         if any(name for team in testing_teams if name.startswith(team)):
             print name
-            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.CircleMarker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup('"+name+pop+"');"
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.CircleMarker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
         else:
-            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup('"+name+pop+"');"
-        #print marker
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
+        print marker
         marker_file.write(marker)
 
 
