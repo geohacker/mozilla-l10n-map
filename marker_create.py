2fbd5ef8998f2848fbe36ccdeadff8e1a8cfe1cf
Sajjad Anwar <me@sajjad.in>
2011-12-05 12:35:50 +0530

    Different markers for testing teams.

--- marker_create.py
+++ marker_create.py
@@ -1,6 +1,7 @@
 import csv
 data = csv.reader(open('mozilla.csv', 'rb'), dialect='excel')
-marker_file = open("markers.js","a")
+marker_file = open("markers_test.js","a")
+testing_teams = ["Akan","Armenian","Kazakh","Kurdish","Luganda","Maithili","Northern Sotho","Oriya","Serbian","Songhai","Tamil","Tamil (Sri Lanka)","Zulu"]
 
 iter =0
 for row in data:
@@ -11,8 +12,12 @@ for row in data:
         lon = row[3]
         name = row[0]
         pop = row[1]
-        marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup('"+name+pop+"');"
-        print marker
+        if any(name for team in testing_teams if name.startswith(team)):
+            print name
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.CircleMarker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup('"+name+pop+"');"
+        else:
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+");map.addLayer(marker"+id+");marker"+id+".bindPopup('"+name+pop+"');"
+        #print marker
         marker_file.write(marker)
 
 
