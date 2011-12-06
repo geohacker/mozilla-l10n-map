9a15ef47f7dbca1d9409ce5469ca5872cf889aae
Sajjad Anwar <me@sajjad.in>
2011-12-06 21:28:19 +0530

    Zoom in/out marker interaction

--- marker_create.py
+++ marker_create.py
@@ -2,7 +2,7 @@ import csv
 data = csv.reader(open('mozilla.csv', 'rb'), dialect='excel')
 marker_file = open("markers_test.js","a")
 testing_teams = ["Akan","Armenian","Kazakh","Kurdish","Luganda","Maithili","Northern Sotho","Oriya","Serbian","Songhai","Tamil","Tamil (Sri Lanka)","Zulu"]
-
+markers=[]
 iter =0
 for row in data:
     iter += 1
@@ -22,11 +22,19 @@ for row in data:
             pass
         if any(name for team in testing_teams if name.startswith(team)):
             print name
-            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+",{icon:betaIcon});map.addLayer(marker"+id+");marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
+            m_name="marker"+id
+            markers.insert(0,m_name)
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+",{icon:betaIcon});marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
         else:
-            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+",{icon:firefoxIcon});map.addLayer(marker"+id+");marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
+            m_name="marker"+id
+            markers.insert(0,m_name)
+            marker = "var markerll"+id+"=new L.LatLng("+lat+","+lon+");var marker"+id+"=new L.Marker(markerll"+id+",{icon:firefoxIcon});marker"+id+".bindPopup(\"<b>"+name+"<br></b><a href=\'"+team_page+"\'>L10n Team Page<br><a href=\'"+dashboard+"\'>Dashboard\");"
         print marker
-        marker_file.write(marker)
+        marker_file.write(marker+";")
+
+for marker in markers:
+    marker_file.write("l10nLayer.addLayer("+marker+")")
+    marker_file.write(";")
 
 
 
